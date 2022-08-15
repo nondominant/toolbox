7c8bc795abc118772288ea166ba0c350981a81ba
123name <123@hotmail.com>
2022-08-15 16:07:59 +1200

    added a script to create self signed cert's using openssl

---
+++ bash/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COUNT=0
+
+echo "$COUNT"
+sleep 1 
+COUNT=1+$COUNT
+echo "$COUNT"
+sleep 1 
+COUNT=1+$COUNT
+echo "$COUNT"
+sleep 1 
+COUNT=1+$COUNT
---
+++ bash/window.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DISPLAY=:0
+xfce4-terminal -e /home/sandworm/Workspace/toolbox/test.sh
